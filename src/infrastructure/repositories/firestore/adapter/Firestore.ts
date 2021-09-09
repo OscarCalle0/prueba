4bc8e8845485ab66fb44d9cf299f2d8dc74b4576
Santiago Gonzalez Johnson <santiago.idarraga@gmail.com>
2021-09-09 17:50:21 -0500

    feat: added project to fs adapter

--- src/infrastructure/repositories/firestore/adapter/Firestore.ts
+++ src/infrastructure/repositories/firestore/adapter/Firestore.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
 import { Firestore } from '@google-cloud/firestore';
+import { GCP_PROJECT } from '@util';
 
-export const firestore = new Firestore();
+export const firestore = new Firestore({ projectId: GCP_PROJECT });
