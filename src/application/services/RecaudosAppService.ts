904a5021aaea6e3c37047855a94e4a5bb16958ba
Julian Ceballos <julianc@coordinadora.com>
2024-10-22 16:14:14 -0500

    fix: separar lógica para bajarle complejidad al método

--- src/application/services/RecaudosAppService.ts
+++ src/application/services/RecaudosAppService.ts
@@ -10,6 +10,8 @@ import { TransaccionesApiClient } from '@infrastructure/api-transacciones';
 import { FirestoreRepository } from '@domain/repository';
 import { INovedades } from '@infrastructure/api/interfaces/INovedades';
 import { NovedadesRepository } from '@domain/repository/NovedadesRepository';
+import { IResponseAliados } from '@infrastructure/api-transacciones/interfaces';
+import { IFirestoreStageResponse } from '@infrastructure/repositories/firestore/interfaces/IFirestoreStageResponse';
 
 @injectable()
 export class RecaudosAppService {
@@ -45,29 +47,36 @@ export class RecaudosAppService {
             delete recaudo.ultimo_error;
             const responseApiTransacciones = await this.recaudoApi.postRecaudosTarea(recaudo);
             if (responseApiTransacciones?.isError) {
-                if (responseApiTransacciones.statusCode < 500) {
-                    const novedad: INovedades = {
-                        id_tipo_novedad: this.ID_TIPO_NOVEDAD_RECAUDO,
-                        detalle: JSON.stringify(recaudo),
-                        descripcion: responseApiTransacciones.message,
-                    };
-                    await this.novedadesRepository.insertar(novedad);
-                    await this.firestoreRepository.updateRecaudoEstado(
-                        recaudo.recaudo_id,
-                        responseApiTransacciones.message,
-                        'error',
-                    );
-                } else {
-                    await this.firestoreRepository.updateRecaudoEstado(
-                        recaudo.recaudo_id,
-                        responseApiTransacciones.message,
-                        'reintentar',
-                    );
-                }
+                this.gestionarErrorApiTransacciones(recaudo, responseApiTransacciones);
                 return Result.okBool(false);
             }
             await this.firestoreRepository.deleteRecaudo(recaudo.recaudo_id);
         }
         return Result.okBool(true);
+    }
+
+    async gestionarErrorApiTransacciones(
+        recaudo: IFirestoreStageResponse,
+        responseApiTransacciones: IResponseAliados,
+    ): Promise<void> {
+        if (responseApiTransacciones.statusCode < 500) {
+            const novedad: INovedades = {
+                id_tipo_novedad: this.ID_TIPO_NOVEDAD_RECAUDO,
+                detalle: JSON.stringify(recaudo),
+                descripcion: responseApiTransacciones.message,
+            };
+            await this.novedadesRepository.insertar(novedad);
+            await this.firestoreRepository.updateRecaudoEstado(
+                recaudo.recaudo_id,
+                responseApiTransacciones.message,
+                'error',
+            );
+        } else {
+            await this.firestoreRepository.updateRecaudoEstado(
+                recaudo.recaudo_id,
+                responseApiTransacciones.message,
+                'reintentar',
+            );
+        }
     }
 }
