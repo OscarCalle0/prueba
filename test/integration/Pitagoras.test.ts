595dc5250dbcd7268437fc84ad805f1acfed4e4b
Nicolas Atara <nsatara@coordinadora.com>
2025-02-12 08:33:51 -0500

    fix: prueba unitaria

--- test/integration/Pitagoras.test.ts
+++ test/integration/Pitagoras.test.ts
@@ -44,7 +44,7 @@ describe('PitagorasRouter', () => {
         });
 
         //const responseBody = JSON.parse(response.payload);
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(201);
         /*expect(responseBody.error).toBe(false);
         expect(responseBody.message).toBe('Registro procesado exitosamente');
         expect(responseBody.code).toBe(200);*/
